27d30a18ac295450fb94ae5092757422acb0f15a
Iamneo <techadmin@iamneo.ai>
2023-08-08 03:52:36 +0000

    Backup Commit

--- reactapp/src/components/Stopwatch/Stopwatch.js
+++ reactapp/src/components/Stopwatch/Stopwatch.js
@@ -39,7 +39,8 @@ function Stopwatch() {
     const hours = Math.floor(timeInSeconds / 3600);
     const minutes = Math.floor((timeInSeconds - (hours * 3600)) / 60);
     const seconds = timeInSeconds - (hours * 3600) - (minutes * 60);
-    return ({`  ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')};
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
   }
 
   return (
